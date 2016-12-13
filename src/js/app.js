a31d00051ba08867e7409c26c596cc7c9ec36eca
Aurélien Goulet <elektrorl@buckethead.local>
2016-12-13 17:13:19 +0100

    tant pis pour les fieldset

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,6 @@
-$(document).foundation();
-Drupal.behaviors.collapsedFieldset = {
+// Ne pas toucher, c'est pour faire fonctionner par défaut Foundation
+Drupal.behaviors.F6 = {
     attach: function(context, settings) {
-        var fieldsetLegend = $('html.js fieldset.collapsed legend');
-        var fieldsetLabel = $(this).find('.fieldset-legend');
-        var contenuFieldset = $(this).siblings();
-        fieldsetLegend.click(function(event) {
-            contenuFieldset.toggle(0);
-            fieldsetLabel.toggleClass('secondary');
-        });
+        $(document).foundation();
     }
 };
